1edb88617752edbce90410eb48bc5f3ab092a84f
potatolauncher <sdnash724@gmail.com>
2022-10-12 17:17:32 -0500

    Begin basic sketch function
    I have started the basic framework for the "sketch" portion of project.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const makeBoxes = function () {
     for (let div = 1; div <= 16; div++){
     const div = document.createElement('div');
-    div.setAttribute('id', div);
+    div.setAttribute('class', 'selection');
     document.getElementById('gridContainer').appendChild(div);
     }
 }
 
 makeBoxes();
+
+const colorBox = document.querySelectorAll('.selection');
+
+colorBox.forEach(selection => {
+    selection.addEventListener('mouseenter', function handleMouseEnter(event) {
+    selection.setAttribute('style', 'background-color: black;');
+    });
+});
+
+
+
 
