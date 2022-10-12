ebf5a8cfa74133ca2c0af1b521dd413d59c939f8
potatolauncher <sdnash724@gmail.com>
2022-10-12 13:37:44 -0500

    Start Grid
    I have almost nearly made the grid to specifications.

---
+++ script.js
@@ -0,0 +1,10 @@
+const makeBoxes = function () {
+    for (let div = 1; div <= 16; div++){
+    const div = document.createElement('div');
+    div.setAttribute('id', 'div' + div);
+    document.getElementById('gridContainer').appendChild(div);
+    }
+}
+
+makeBoxes();
+
